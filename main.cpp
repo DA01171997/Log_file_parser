2c308d6084741bb1a8e91bcff6910acdfe72659d
DA01171997 <duy.ado97@gmail.com>
2017-11-22 16:57:14 -0800

    Add hexToBinary to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,55 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using std::cout;
 using std::endl;
+using std::string;
+
+string hexTobinary(string a) {				// function that take a hex string and returns a binary string
+	string temp="";
+	char * c;
+	for (int i = 0; i < a.length(); i++) {
+		c = &a[i];
+		switch (*c) {
+		case '0': temp.append("0000");
+			break;
+		case '1': temp.append("0001");
+			break;
+		case '2': temp.append("0010");
+			break;
+		case '3': temp.append("0011");
+			break;
+		case '4': temp.append("0100");
+			break;
+		case '5': temp.append("0101");
+			break;
+		case '6': temp.append("0110");
+			break;
+		case '7': temp.append("0111");
+			break;
+		case '8': temp.append("1000");
+			break;
+		case '9': temp.append("1001");
+			break;
+		case 'A': temp.append("1010");
+			break;
+		case 'B': temp.append("1011");
+			break;
+		case 'C': temp.append("1100");
+			break;
+		case 'D': temp.append("1101");
+			break;
+		case 'E': temp.append("1110");
+			break;
+		case 'F': temp.append("1111");
+			break;
+		}
+	}
+	return temp;
+}
+
+
 int main() {
 	std::ifstream inFile;
 	std::ofstream outFile;
@@ -18,8 +65,9 @@ int main() {
 		}
 	}
 	catch (std::runtime_error &e) {								//exception catch for file open
-		cout << e.what()<<endl;
+		cout << e.what() << endl;
 	}
 	inFile.close();
+	system("PAUSE");
 	return 0;
 }
