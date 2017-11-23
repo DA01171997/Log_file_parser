291ae0734b6e0b14fe552541df881444e4a9f862
Jason Lieu <jasonlieuthefirst@gmail.com>
2017-11-22 18:35:12 -0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include "Line.cpp"
 using std::cout;
 using std::endl;
+using std::string;
+using std::istringstream;
+
 int main() {
 	std::ifstream inFile;
 	std::ofstream outFile;
@@ -18,8 +24,45 @@ int main() {
 		}
 	}
 	catch (std::runtime_error &e) {								//exception catch for file open
-		cout << e.what()<<endl;
+		cout << e.what() << endl;
+	}
+
+	string line;
+	int lineCount = 1;			//current line
+	getline(inFile, line);		//skip first line
+	while (!inFile.eof())
+	{
+		lineCount++;
+		getline(inFile, line);
+		istringstream iss(line);
+		string useless, alsoUseless, time, uselessToo, veryUseless, superUseless, command, data, stillUseless, ReadWrite;
+		if (!(iss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite)) { break; }
+		if (command == "40000810" || command == "40000C18")
+		{
+
+			Line CommandLine = Line(command, ReadWrite, data);
+			cout << "Line " << lineCount << ": " << CommandLine.getReadWrite() << " " << CommandLine.getCommand() << ": " << CommandLine.getWordCount() << " words" << endl;
+			int WordCount = 0;
+			while (WordCount < CommandLine.getWordCount())			//doesn't work yet b/c need to convert data hex to base10 and divide by 2
+			{
+				getline(inFile, line);
+				if (!(iss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite)) { break; }
+				Line SubLine = Line(command, ReadWrite, data);
+				for (int subLineWord = 0; subLineWord < 2; subLineWord++) {
+					cout << "Line " << lineCount << ": word" << WordCount << ": ";
+					//HEX TO BINARY
+					//if(subLineWord == 0)
+					//LOOK THROUGH FIRST 8 BITS
+					//PRINT ACCORDING TO CHART
+					//else
+					//LOOK THROUGH LAST 8 BITS
+					//PRINT ACCORDING TO CHART
+				}
+			}
+			cout << endl;
+		}
 	}
 	inFile.close();
+	system("pause");
 	return 0;
 }
