16ad55c13665045a0d5c3e3389f0704f5bd308e7
Jason Lieu <jasonlieuthefirst@gmail.com>
2017-11-29 11:03:57 -0800

    update main

--- main.cpp
+++ main.cpp
@@ -14,11 +14,14 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::istringstream;
-int hexTodecimal(string);
-string hexTobinary(string);
+using std::ifstream;
 int main() {
-	std::ifstream inFile;
+	ifstream inFile;
+	ifstream Backup;						//used to get data from next line
 	std::ofstream outFile;
+	char * cTemp;
+
+
 	try {
 		inFile.open("test_data.log");							//open log file
 		if (!inFile) {									//check if log file opened
@@ -29,148 +32,235 @@ int main() {
 		cout << e.what() << endl;
 	}
 
+
+	try {
+		Backup.open("test_data.log");							//open log file
+		if (!Backup) {									//check if log file opened
+			throw std::runtime_error("can't open inFile");
+		}
+	}
+	catch (std::runtime_error &e) {								//exception catch for file open
+		cout << e.what() << endl;
+	}
 	string line;
+	string nextLine;
 	int lineCount = 1;			//current line
 	getline(inFile, line);		//skip first line
+	getline(Backup, nextLine);
+	getline(Backup, nextLine);
+
 	while (!inFile.eof())
 	{
+		bool reversed = false;
 		lineCount++;
 		getline(inFile, line);
+		getline(Backup, nextLine);
+
 		istringstream iss(line);
 		string useless, alsoUseless, time, uselessToo, veryUseless, superUseless, command, data, stillUseless, ReadWrite;
 		if (!(iss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite)) { break; }
-		if (command == "40000810" || command == "40000C18")
-		{
 
-			Line CommandLine = Line(command, ReadWrite, data);
-			cout << "Line " << lineCount << ": " << CommandLine.getReadWrite() << " " << CommandLine.getCommand() << ": " << hexTodecimal(CommandLine.getData()) / 2 << " words" << endl;
-			int WordCount = 0;
-			while (WordCount < hexTodecimal(CommandLine.getData()) / 2)			//doesn't work yet b/c need to convert data hex to base10 and divide by 2
-			{
-				getline(inFile, line);
-				lineCount++;
-				iss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite;
-				Line SubLine = Line(command, ReadWrite, data);
-				string DataBinary = hexTobinary(SubLine.getData());
-				for (int subLineWord = 0; subLineWord < 2; subLineWord++) {
-					cout << "Line " << lineCount << ": word" << WordCount << ": ";
-					string HalfBinary;
-					if (subLineWord == 0) { /* first half of DataBinary is put into HalfBinary*/ }
-					if (subLineWord == 1) {/*second half of DataBinary is put into HalfBinary*/ }
+		istringstream issss(nextLine);
+		string a, b, nextTime, c, d, e, nextCommand;
+		issss >> a >> b >> nextTime >> c >> d >> e >> nextCommand;
 
-					//HEX TO BINARY
-					//if(subLineWord == 0)
-					//LOOK THROUGH FIRST 8 BITS
-					//PRINT ACCORDING TO CHART
-					//else
-					//LOOK THROUGH LAST 8 BITS
-					//PRINT ACCORDING TO CHART
-
-					if (WordCount == 0) {
-						cout << "Line " << lineCount << ": word" << WordCount << ": Rec_Ctrl = ";
-						if ( /*bit 14-13 is 00*/) { cout << 0 << endl; }
-						if ( /*bit 14-13 is 10*/) { cout << 2 << endl; }
-						if ( /*bit 14-13 is 11*/) { cout << 3 << endl; }
-					}
-
-
-
-					cout << endl;
-					WordCount++;
-				}
+		if (command == "40000C18" || command == "40000810") {
+			if (nextCommand != "40000818" && nextCommand != "40000C20") {
+				reversed = true;
 			}
-			cout << endl;
+			else reversed = false;
+			if (!reversed)
+			{
+				Line CommandLine = Line(command, ReadWrite, data);
+				cout << "Line " << lineCount << ": " << CommandLine.getReadWrite() << " " << CommandLine.getCommand() << ": " << hexTodecimal(CommandLine.getData()) / 2 << " words" << endl;
+				int WordCount = 0;
+				while (WordCount < hexTodecimal(CommandLine.getData()) / 2)			//doesn't work yet b/c need to convert data hex to base10 and divide by 2
+				{
+					getline(inFile, line);
+					getline(Backup, nextLine);
+					lineCount++;
+					istringstream isss(line);
+					isss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite;
+					Line SubLine = Line(command, ReadWrite, data);
+					string DataBinary = hexTobinary(SubLine.getData());
+					for (int subLineWord = 0; subLineWord < 2; subLineWord++) {
+						string HalfBinary = "";
+						if (subLineWord == 0) {
+							for (int i = 0; i < 16; i++) {
+								cTemp = &DataBinary[i];
+								HalfBinary += (*cTemp);
+							}
+						}
+						if (subLineWord == 1) {
+							for (int i = 16; i < 32; i++) {
+								cTemp = &DataBinary[i];
+								HalfBinary += (*cTemp);
+							}
+						}
+						reverse(HalfBinary.begin(), HalfBinary.end());
+						//		cout << endl << HalfBinary << "              " << binaryToDecimal(HalfBinary.substr(13, 2))<<endl;
+						switch (WordCount) {
+						case 0:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Rec_ctrl = ";
+							if (binaryToDecimal(HalfBinary.substr(13, 2)) == 0) { cout << "0 (no recording)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 2)) == 2) { cout << "2 (no processing)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 2)) == 3) { cout << "3 (processing & recording)" << endl; }
+							break;
+						case 1:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Cmd_Type = ";
+							if (binaryToDecimal(HalfBinary.substr(13, 3)) == 4) { cout << "4 (Type A)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 3)) == 5) { cout << "5 (Type B)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 3)) == 6) { cout << "6 (Type C)" << endl; }
+							else cout << "1 (unknown)" << endl;
+							break;
+						case 4:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Rec_Raw = ";
+							if (binaryToDecimal(HalfBinary.substr(0, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(0, 1)) == 1) { cout << "1 (enable)" << endl; }
+							break;
+						case 5:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Cmd_ID = " << binaryToDecimal(HalfBinary.substr(0, 7)) << endl;
+							break;
+						case 10:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Num_Responses = " << binaryToDecimal(HalfBinary.substr(11, 5)) << endl;
+							break;
+						case 15:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Reset_Enabled = ";
+							if (binaryToDecimal(HalfBinary.substr(2, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(2, 1)) == 1) { cout << "1 (enable)" << endl; }
+							break;
+						case 22:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Direction = ";
+							if (binaryToDecimal(HalfBinary.substr(3, 1)) == 0) { cout << "0 (Right)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(3, 1)) == 1) { cout << "1 (Left)" << endl; }
+							break;
+						case 32:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Num_Samples = " << binaryToDecimal(HalfBinary.substr(0, 15)) << endl;
+							break;
+						case 37:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Parity = ";
+							if (binaryToDecimal(HalfBinary.substr(15, 1)) == 0) { cout << "0 (even)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(15, 1)) == 1) { cout << "1 (odd)" << endl; }
+							break;
+						case 38:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Test = ";
+							if (binaryToDecimal(HalfBinary.substr(14, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(14, 1)) == 0) { cout << "1 (enable)" << endl; }
+							break;
+						case 40:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Ctrl_Enable = ";
+							if (binaryToDecimal(HalfBinary.substr(7, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(7, 1)) == 0) { cout << "0 (disable)" << endl; }
+							break;
+						case 41:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Code = " << binaryToDecimal(HalfBinary.substr(8, 7)) << endl;
+							break;
+						}
+						WordCount++;
+					}
+				}
+				cout << endl;
+			}
+			if (reversed)
+			{
+
+				Line CommandLine = Line(command, ReadWrite, data);
+				cout << "Line " << lineCount << ": " << CommandLine.getReadWrite() << " " << CommandLine.getCommand() << ": " << hexTodecimal(CommandLine.getData()) / 2 << " words" << endl;
+				int WordCount = (hexTodecimal(CommandLine.getData()) / 2) - 1;
+				while (WordCount > 0)			//doesn't work yet b/c need to convert data hex to base10 and divide by 2
+				{
+					getline(inFile, line);
+					getline(Backup, nextLine);
+
+					lineCount++;
+					istringstream isss(line);
+					isss >> useless >> alsoUseless >> time >> uselessToo >> veryUseless >> superUseless >> command >> data >> stillUseless >> ReadWrite;
+					Line SubLine = Line(command, ReadWrite, data);
+					string DataBinary = hexTobinary(SubLine.getData());
+					for (int subLineWord = 0; subLineWord < 2; subLineWord++) {
+						string HalfBinary = "";
+						if (subLineWord == 1) {
+							for (int i = 0; i < 16; i++) {
+								cTemp = &DataBinary[i];
+								HalfBinary += (*cTemp);
+							}
+						}
+						if (subLineWord == 0) {
+							for (int i = 16; i < 32; i++) {
+								cTemp = &DataBinary[i];
+								HalfBinary += (*cTemp);
+							}
+						}
+						reverse(HalfBinary.begin(), HalfBinary.end());
+
+						//		cout << endl << HalfBinary << "              "<< binaryToDecimal(HalfBinary.substr(13, 2))<<endl;
+
+						switch (WordCount) {
+						case 0:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Rec_ctrl = ";
+							if (binaryToDecimal(HalfBinary.substr(13, 2)) == 0) { cout << "0 (no recording)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 2)) == 2) { cout << "2 (no processing)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 2)) == 3) { cout << "3 (processing & recording)" << endl; }
+							break;
+						case 1:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Cmd_Type = ";
+							if (binaryToDecimal(HalfBinary.substr(13, 3)) == 4) { cout << "4 (Type A)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 3)) == 5) { cout << "5 (Type B)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(13, 3)) == 6) { cout << "6 (Type C)" << endl; }
+							else cout << "1 (unknown)" << endl;
+							break;
+						case 4:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Rec_Raw = ";
+							if (binaryToDecimal(HalfBinary.substr(0, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(0, 1)) == 1) { cout << "1 (enable)" << endl; }
+							break;
+						case 5:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Cmd_ID = " << binaryToDecimal(HalfBinary.substr(0, 7)) << endl;
+							break;
+						case 10:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Num_Responses = " << binaryToDecimal(HalfBinary.substr(11, 5)) << endl;
+							break;
+						case 15:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Reset_Enabled = ";
+							if (binaryToDecimal(HalfBinary.substr(2, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(2, 1)) == 1) { cout << "1 (enable)" << endl; }
+							break;
+						case 22:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Direction = ";
+							if (binaryToDecimal(HalfBinary.substr(3, 1)) == 0) { cout << "0 (Right)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(3, 1)) == 1) { cout << "1 (Left)" << endl; }
+							break;
+						case 32:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Num_Samples = " << binaryToDecimal(HalfBinary.substr(0, 15)) << endl;
+							break;
+						case 37:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Parity = ";
+							if (binaryToDecimal(HalfBinary.substr(15, 1)) == 0) { cout << "0 (even)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(15, 1)) == 1) { cout << "1 (odd)" << endl; }
+							break;
+						case 38:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Test = ";
+							if (binaryToDecimal(HalfBinary.substr(14, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(14, 1)) == 0) { cout << "1 (enable)" << endl; }
+							break;
+						case 40:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Ctrl_Enable = ";
+							if (binaryToDecimal(HalfBinary.substr(7, 1)) == 0) { cout << "0 (disable)" << endl; }
+							else if (binaryToDecimal(HalfBinary.substr(7, 1)) == 0) { cout << "0 (disable)" << endl; }
+							break;
+						case 41:
+							cout << "Line " << lineCount << ": word " << WordCount << ": Code = " << binaryToDecimal(HalfBinary.substr(8, 7)) << endl;
+							break;
+						}
+						WordCount--;
+					}
+				}
+				cout << endl;
+			}
 		}
 	}
 	inFile.close();
+	Backup.close();
 	system("pause");
 	return 0;
 }
-/*
-int hexTodecimal(string a) {
-int stringNum = a.length();
-int counter = 0;
-double total = 0;
-char * c;
-for (int i = (stringNum - 1); i > -1; i--) {
-c = &a[i];
-switch (*c) {
-case '0': total += (pow(16, counter) * 0);
-break;
-case '1': total += (pow(16, counter) * 1);
-break;
-case '2': total += (pow(16, counter) * 2);
-break;
-case '3': total += (pow(16, counter) * 3);
-break;
-case '4': total += (pow(16, counter) * 4);
-break;
-case '5': total += (pow(16, counter) * 5);
-break;
-case '6': total += (pow(16, counter) * 6);
-break;
-case '7': total += (pow(16, counter) * 7);
-break;
-case '8': total += (pow(16, counter) * 8);
-break;
-case '9': total += (pow(16, counter) * 9);
-break;
-case 'A': total += (pow(16, counter) * 10);
-break;
-case 'B': total += (pow(16, counter) * 11);
-break;
-case 'C': total += (pow(16, counter) * 12);
-break;
-case 'D': total += (pow(16, counter) * 13);
-break;
-case 'E': total += (pow(16, counter) * 14);
-break;
-case 'F': total += (pow(16, counter) * 15);
-break;
-}
-counter++;
-}
-return (int)total;
-}
-string hexTobinary(string a) {				// function that take a hex string and returns a binary string
-string temp = "";
-char * c;
-for (int i = 0; i < a.length(); i++) {
-c = &a[i];
-switch (*c) {
-case '0': temp.append("0000");
-break;
-case '1': temp.append("0001");
-break;
-case '2': temp.append("0010");
-break;
-case '3': temp.append("0011");
-break;
-case '4': temp.append("0100");
-break;
-case '5': temp.append("0101");
-break;
-case '6': temp.append("0110");
-break;
-case '7': temp.append("0111");
-break;
-case '8': temp.append("1000");
-break;
-case '9': temp.append("1001");
-break;
-case 'A': temp.append("1010");
-break;
-case 'B': temp.append("1011");
-break;
-case 'C': temp.append("1100");
-break;
-case 'D': temp.append("1101");
-break;
-case 'E': temp.append("1110");
-break;
-case 'F': temp.append("1111");
-break;
-}
-}
-return temp;
-}*/
